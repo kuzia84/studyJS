d8e12888e1e4517cea8ea821654df421cbe8ca3e
Vasiliy Chumachenco <kuzia84@gmail.com>
2020-10-14 13:16:49 +0300

    lesson05 difficult

--- difficult/script.js
+++ difficult/script.js
@@ -1,20 +1,16 @@
 "use strict";
-let foo = function (data) {
-  if (typeof data === "string") {
-    //проверяем, является ли data строкой
-    let str = data.trim(); //присваеваем str строку из data и убираем пробелы по караям строки
-    if (str.length > 30) {
-      // проверяем длину str, ели она больше 30
-      str = str.slice(0, 29) + "..."; // то обрезаем все после 30 символа и добавляем троеточие
-    }
-    return str; //возвращаем str
-  } else {
-    return "передайте строку в функцию";
+let arr = ["98754", "54645", "213545", "4687654", "465456", "24545", "89795"];
+arr.forEach((number) => {
+  if (number.charAt(0) === "2" || number.charAt(0) === "4") {
+    console.log(number);
   }
-};
-
-console.log(
-  foo(
-    "      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum commodi incidunt nihil natus quia libero modi? Iste harum aperiam nostrum cumque doloremque vitae, minus fugit quaerat. Exercitationem illo quam autem. "
-  )
-);
+});
+let n = 100;
+nextPrime: for (let i = 2; i < n; i++) {
+  for (let j = 2; j < i; j++) {
+    if (i % j == 0) {
+      continue nextPrime;
+    }
+  }
+  console.log(i, `Делители этого числа: 1 и ${i}`);
+}
