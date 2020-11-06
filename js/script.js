a039f6e040c568fd83e125ca421cd62b23d51f60
kuzia84 <45180124+kuzia84@users.noreply.github.com>
2020-11-07 00:40:43 +0200

    difficult

--- js/script.js
+++ js/script.js
@@ -19,22 +19,37 @@ class Todo {
     this.todoCompleted.textContent = "";
     this.todoData.forEach(this.createItem, this);
     this.addToStorage();
+    setTimeout(() => {
+      document.querySelector(".animated").style.opacity = 1;
+      this.todoData.forEach((element) => {
+        element.animate = false;
+      });
+    }, 500);
   }
 
   createItem(todo) {
+    console.log("todo: ", todo);
     const li = document.createElement("li");
     li.classList.add("todo-item");
     li.key = todo.key;
+    li.animate = todo.animate;
+    li.style.cssText = "transition: all 0.5s ease;";
     li.insertAdjacentHTML(
       "beforeend",
       `
       <span class="text-todo">${todo.value}</span>
       <div class="todo-buttons">
+        <button class="todo-edit"></button>
         <button class="todo-remove"></button>
         <button class="todo-complete"></button>
       </div>
       `
     );
+
+    if (todo.animate === true) {
+      li.classList.add("animated");
+      li.style.opacity = 0;
+    }
 
     if (todo.completed) {
       this.todoCompleted.append(li);
@@ -50,6 +65,7 @@ class Todo {
         value: this.input.value,
         completed: false,
         key: this.generateKey(),
+        animate: false,
       };
       this.todoData.set(newTodo.key, newTodo);
       this.render();
@@ -65,20 +81,49 @@ class Todo {
     );
   }
 
-  deleteItem(key) {
-    this.todoData.forEach((element) => {
-      if (element.key === key) {
-        this.todoData.delete(element.key);
-        this.render();
+  editItem(item) {
+    //сделал это сперва через contenteditable="true", но потом решил переделать через input + button
+    const editTodo = `
+      <input type="text" class="new-text" style="border:1px solid;max-width:300px"/>
+      <button class="new-text-btn" type="submit">Принять</button>
+    `;
+    item.insertAdjacentHTML("afterbegin", editTodo);
+    const newText = item.querySelector(".new-text"),
+      newTextBtn = item.querySelector(".new-text-btn");
+
+    newTextBtn.addEventListener("click", () => {
+      if (newText.value.trim()) {
+        this.todoData.forEach((element) => {
+          if (element.key === item.key) {
+            element.value = newText.value.trim();
+            this.render();
+          }
+        });
       }
     });
   }
 
-  completedItem(key) {
+  deleteItem(item) {
     this.todoData.forEach((element) => {
-      if (element.key === key) {
+      if (element.key === item.key) {
+        this.todoData.delete(element.key);
+        item.style.opacity = 0;
+        setTimeout(() => {
+          this.render();
+        }, 500);
+      }
+    });
+  }
+
+  completedItem(item) {
+    this.todoData.forEach((element) => {
+      if (element.key === item.key) {
         element.completed = !element.completed;
-        this.render();
+        element.animate = true;
+        item.style.opacity = 0;
+        setTimeout(() => {
+          this.render();
+        }, 500);
       }
     });
   }
@@ -90,10 +135,13 @@ class Todo {
 
       if (target.matches(".todo-complete")) {
         target = target.closest(".todo-item");
-        this.completedItem(target.key);
+        this.completedItem(target);
       } else if (target.matches(".todo-remove")) {
         target = target.closest(".todo-item");
-        this.deleteItem(target.key);
+        this.deleteItem(target);
+      } else if (target.matches(".todo-edit")) {
+        target = target.closest(".todo-item");
+        this.editItem(target);
       }
     });
   }
